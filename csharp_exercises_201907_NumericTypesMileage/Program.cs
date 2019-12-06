aa44632aa78bbe84a61f3674362b3f03798fa58b
msunkist99 <msunkist99@gmail.com>
2019-12-06 15:07:09 -0600

    Add project files.

---
+++ csharp_exercises_201907_NumericTypesMileage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csharp_exercises_201907_NumericTypesMileage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of miles driven - ");
+            float numberOfMiles = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter amount of gas consumed - ");
+            float gallonsOfGas = float.Parse(Console.ReadLine());
+
+            float mileage = numberOfMiles / gallonsOfGas;
+
+            Console.WriteLine("Mileage (miles per gallon of gas) is - " + mileage);
+            Console.WriteLine("Mileage (miles per gallon of gas) is - " + (numberOfMiles / gallonsOfGas));
+
+            Console.ReadLine();
+        }
+    }
+}
